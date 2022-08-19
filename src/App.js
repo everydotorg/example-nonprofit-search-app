366f255578e3e36e293f2ecfa82509107d58ecd3
Alexander Worley <alexander@every.org>
2022-08-19 15:03:49 -0700

    Captured user input in search bar.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  function handleSearchInputChange(event) {
+    setSearchValue(event.target.value);
+  }
   return (
     <div className="App">
-      <input placeholder="Search for a nonprofit!"/>
+      <form>
+      <input placeholder="Search for a nonprofit!" type="search" name="search" value={searchValue} onChange={handleSearchInputChange}/>
+      </form>
+      <p>{searchValue}</p>
     </div>
   );
 }
