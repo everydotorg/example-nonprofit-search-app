d22a771a63c0177714d924bbe60c03e215ad1159
Alexander Worley <alexander@every.org>
2022-08-25 16:00:41 -0700

    Implemented completed search bar that returns results from Every.org API in bulitted list

--- src/App.js
+++ src/App.js
@@ -5,13 +5,39 @@ function App() {
   const [searchValue, setSearchValue] = useState("");
   function handleSearchInputChange(event) {
     setSearchValue(event.target.value);
+    if (event.target.value === "") {
+      setSearchResults([]);
+    } else {
+      setNewSearch(event.target.value);
+    }
+  }
+  const [searchResults, setSearchResults] = useState([]);
+  function setNewSearch(newSearchValue) {
+    fetch("https://partners.every.org/v0.2/search/" + newSearchValue + "?apiKey=5a2be079dc5a64d56dfae3786abe4543")
+    .then(handleResult)
+    .then(handleData);
+  }
+  function handleResult(result) {
+    return result.json();
+  }
+  function handleData(data) {
+    setSearchResults(data.nonprofits);
+  }
+  function handleNonprofit(searchResult) {
+    console.log(searchResult);
+    return (
+    <li key={searchResult.name}>
+      <a href={searchResult.profileUrl + "#donate"}>{searchResult.name}</a>
+      <ul>{searchResult.description}</ul>    
+    </li>
+    );
   }
   return (
     <div className="App">
       <form>
       <input placeholder="Search for a nonprofit!" type="search" name="search" value={searchValue} onChange={handleSearchInputChange}/>
       </form>
-      <p>{searchValue}</p>
+      <ul>{searchResults.map(handleNonprofit)}</ul>
     </div>
   );
 }
